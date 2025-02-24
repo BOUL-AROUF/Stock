617c09c95febfcc36bd9860f6ebf52fb06eee4ae
BOUL-AROUF <boularoufgit@gmail.com>
2025-02-24 15:46:37 +0100

    express.js

--- express.js
+++ express.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'src', 'index.html')));
 
 // Your login endpoint
 app.post('/login', (req, res) => {
